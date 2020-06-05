e24dee994541e3da7ea993d986b54a88b19c77e3
mseeli <markus@seeli.eu>
2020-06-05 07:44:47 +0200

    title on picture set via JS

--- src/app.js
+++ src/app.js
@@ -5,3 +5,9 @@ function getTitle() {
 function showUrl() {
   alert(window.location.href);
 }
+
+function setTitle() {
+  const titleElement = document.getElementById("titleonpic");
+  titleElement.innerHTML = document.title;
+  console.log(titleElement.innerHTML);
+}
